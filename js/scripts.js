8736d61771ba8ea2a18cb8c462b51ab9afe8498f
paaulinaaas <psikora089@gmail.com>
2017-07-02 18:02:23 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-var a = -4.5;
-var b = 7;
-var value = (a * a) + (2 * a * b) - (b * b)
-console.log(value)
-
-if (value > 0) {
-	console.log('wynik jest dodatni')
-}
-else if (value == 0) {
-	console.log('wynik rowny jest 0')
-}
-else {
-	console.log('wynik jest ujemny')
-}
+ var a = 15;
+ var h = 10;
+ var triangleArea = a*h/2
+ console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
